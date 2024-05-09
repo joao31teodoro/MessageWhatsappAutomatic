a23af5d674258c5e3e0f8adbc6d2aa4c5cee6a4a
João Teodoro (Teo) <51130248+joao31teodoro@users.noreply.github.com>
2024-05-09 16:02:56 -0300

    Add files via upload

---
+++ enviaMensagemWhats.py
@@ -0,0 +1,18 @@
+import pywhatkit
+import pyautogui
+import time
+
+def enviar_mensagem_whatsapp(numeros, mensagem):
+    for numero in numeros:
+        pywhatkit.sendwhatmsg_instantly(f"+{numero}", mensagem, wait_time=20)  # Envia a mensagem imediatamente com um tempo de espera de 5 segundos entre cada mensagem
+        time.sleep(10)  # Aguarda 10 segundos após abrir o WhatsApp Web
+        pyautogui.press('enter')  # Simula a tecla Enter para enviar a mensagem
+
+# Lista de números de telefone e mensagem padrão
+
+numeros = ['5519111111111', '5511922223344']  # Substitua pelos números de telefone
+mensagem = '''  Sua mensagem aqui
+                Sua mensagem aqui
+                Sua mensagem aqui'''  # Mensagem que deseja enviar
+
+enviar_mensagem_whatsapp(numeros, mensagem)
